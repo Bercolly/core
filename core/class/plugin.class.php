efd0bb388e8978c2fd1a5eb784f4f410c797664f
Bercolly <colvertai@yahoo.fr>
2020-02-21 12:42:32 +0100

    typo

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -279,7 +279,7 @@ class plugin {
 				}
 				if (!$ok) {
 					$message = __('Attention le plugin ', __FILE__) . ' ' . $plugin->getName();
-					$message .= __(' n\'a recu de message depuis ', __FILE__) . $heartbeat . __(' min', __FILE__);
+					$message .= __(' n\'a reçu de message depuis ', __FILE__) . $heartbeat . __(' min', __FILE__);
 					$logicalId = 'heartbeat' . $plugin->getId();
 					message::add($plugin->getId(), $message, '', $logicalId);
 					if ($plugin->getHasOwnDeamon() && config::byKey('heartbeat::restartDeamon::' . $plugin->getId(), 'core', 0) == 1) {
